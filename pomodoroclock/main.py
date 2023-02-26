c73eda879d8cb48329fb81f0e853b3706c372cfa
Dhruv  Bhasin <dhruvbhasin0007@gmail.com>
2023-02-26 18:17:10 +0530

    test commit 3

--- pomodoroclock/main.py
+++ pomodoroclock/main.py
@@ -9,7 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+print("hello")
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
@@ -19,12 +19,28 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("POMODORO")
+title_label = Label(text="TIMER")
+title_label.config(text="TIMER",fg=GREEN,bg=YELLOW,font=(FONT_NAME,35,"bold"))
+title_label.grid(row=0,column=1)
+
+
+
+
 window.config(padx=100 , pady=50,bg=YELLOW)
 canvas = Canvas(width=200 , height= 224 , bg=YELLOW,highlightthickness=0)
 tomato_img = PhotoImage(file = "tomato.png")
 canvas.create_image(100,112, image=tomato_img)
-canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME,35))
+canvas.grid(row=1,column=1)
+
+start_button = Button(text="start",highlightthickness=0)
+
+start_button.grid(row=2,column=0)
+
+end_button = Button(text="reset",highlightthickness=0,bg=YELLOW)
+
+end_button.grid(row=2,column=2)
+
 
 
 
